190b6087985d19c7942246b3f730b1b6cc8a4825
Artem Levin <leoartemlev@yandex.ru>
2023-02-11 17:56:32 +0500

    test for account service

--- ItPlanet.UnitTests/ServicesTests/AccountServiceTests.cs
+++ ItPlanet.UnitTests/ServicesTests/AccountServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoFixture.Xunit2;
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using FluentAssertions;
 using ItPlanet.Database;
 using ItPlanet.Exceptions;
 using ItPlanet.Models;
@@ -11,11 +13,24 @@ public class AccountServiceTests
 {
     [Theory]
     [AutoMoqData]
-    public void GetAccountById_NoAccount_ThrowAccountNotFoundException(
+    public void GetAccountAsync_NoAccount_ThrowAccountNotFoundException(
         [Frozen] Mock<IRepository<AccountModel, int>> repositoryMock, AccountService sut)
     {
         repositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((AccountModel)default!);
 
         Assert.ThrowsAsync<AccountNotFoundException>(async () => await sut.GetAccountAsync(default!));
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccountAsync_ThereIdAccount_ReturnsAccount(
+        [Frozen] Mock<IRepository<AccountModel, int>> repositoryMock, AccountService sut)
+    {
+        var expected = new Fixture().Create<AccountModel>();
+        repositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(expected);
+
+        var account = await sut.GetAccountAsync(default);
+
+        account.Should().BeSameAs(expected);
     }
 }
