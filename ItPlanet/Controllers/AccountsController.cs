4946f154fce6e80cc390320c5c9d73ede82d10e6
Artem Levin <leoartemlev@yandex.ru>
2023-02-11 19:10:15 +0500

    added logging

--- ItPlanet/Controllers/AccountsController.cs
+++ ItPlanet/Controllers/AccountsController.cs
@@ -9,25 +9,30 @@ namespace ItPlanet.Controllers;
 public class AccountsController : ControllerBase
 {
     private readonly IAccountService _accountService;
+    private readonly ILogger<AccountsController> _logger;
 
-    public AccountsController(IAccountService accountService)
+    public AccountsController(IAccountService accountService, ILogger<AccountsController> logger)
     {
         _accountService = accountService;
+        _logger = logger;
     }
 
     [HttpGet("{accountId:int}")]
     public async Task<IActionResult> GetAccount(int? accountId)
     {
+        _logger.LogInformation($"Get {nameof(GetAccount)} request");
         if (accountId is null or <= 0)
             return BadRequest();
 
         try
         {
             var account = await _accountService.GetAccountAsync(accountId.Value);
+            _logger.LogInformation("Account with id {AccountId} successfully found", accountId);
             return Ok(account);
         }
         catch (AccountNotFoundException e)
         {
+            _logger.LogWarning(e, "Account was not found");
             return NotFound();
         }
         
