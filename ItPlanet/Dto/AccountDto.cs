2ac31966e29fc8eee757aa508cf2b2ab41734936
Artem Levin <leoartemlev@yandex.ru>
2023-02-12 19:58:24 +0500

    created AccountDto.cs

---
+++ ItPlanet/Dto/AccountDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItPlanet.Dto;
+
+public class AccountDto
+{
+    [Required] [MinLength(1)] public string FirstName { get; set; } = default!;
+
+    [Required] [MinLength(1)] public string LastName { get; set; } = default!;
+
+    [Required] [MinLength(1)] public string Email { get; set; } = default!;
+
+    [Required] [MinLength(1)] public string Password { get; set; } = default!;
+}
