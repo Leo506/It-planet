2ce63eca7bf6ff4a9bf4d0adb77ccfcd990b5f6a
Artem Levin <leoartemlev@yandex.ru>
2023-02-15 19:59:30 +0500

    create base interface for repository

---
+++ ItPlanet.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace ItPlanet.Infrastructure.Repositories;
+
+public interface IRepository<TModel, in TId>
+{
+    Task<TModel?> GetAsync(TId id);
+
+    Task<List<TModel>> GetAllAsync();
+
+    Task<TModel> CreateAsync(TModel model);
+
+    Task CreateRangeAsync(IEnumerable<TModel> models);
+
+    Task<TModel> UpdateAsync(TModel model);
+
+    Task UpdateRangeAsync(IEnumerable<TModel> models);
+
+    Task DeleteAsync(TModel model);
+
+    Task DeleteRangeAsync(IEnumerable<TModel> models);
+}
