4166acb2ee1bf71766507c0fc995da702c82d789
Artem Levin <leoartemlev@yandex.ru>
2023-02-16 22:29:01 +0500

    finish unit tests for GetAccount

--- ItPlanet.UnitTests/ControllersTests/AccountControllerTests.cs
+++ ItPlanet.UnitTests/ControllersTests/AccountControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text;
 using AutoFixture.Xunit2;
 using FluentAssertions;
+using ItPlanet.Exceptions;
+using ItPlanet.Infrastructure.Services.Account;
 using ItPlanet.Infrastructure.Services.Auth;
 using ItPlanet.Web.Controllers;
 using Microsoft.AspNetCore.Http;
@@ -33,10 +35,8 @@ public class AccountControllerTests
     {
         authServiceMock.Setup(x => x.TryLogin(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
 
-        var encodedHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes("test@test.com:password"));
-        var headerValue = $"Basic {encodedHeaderValue}";
-
-        httpContext.Setup(x => x.Request.Headers.Authorization).Returns(new StringValues(headerValue));
+        httpContext.Setup(x => x.Request.Headers.Authorization)
+            .Returns(new StringValues(GetAuthorizationHeaderValue()));
 
         sut.ControllerContext = new ControllerContext
         {
@@ -45,5 +45,89 @@ public class AccountControllerTests
 
         var response = await sut.GetAccount(1).ConfigureAwait(false);
         response.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccount_AccountIdIsNull_Returns400([Frozen] Mock<HttpContext> httpContext,
+        [Greedy] AccountsController sut)
+    {
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var response = await sut.GetAccount(null);
+        response.Should().BeOfType<BadRequestResult>();
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccount_AccountIdIsLessThanZero_Returns400([Frozen] Mock<HttpContext> httpContext,
+        [Greedy] AccountsController sut)
+    {
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var response = await sut.GetAccount(-1);
+        response.Should().BeOfType<BadRequestResult>();
+    }
+    
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccount_AccountIdIsZero_Returns400([Frozen] Mock<HttpContext> httpContext,
+        [Greedy] AccountsController sut)
+    {
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var response = await sut.GetAccount(0);
+        response.Should().BeOfType<BadRequestResult>();
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccount_IncorrectAuthData_Returns401([Frozen] Mock<IHeaderAuthenticationService> authService,
+        [Frozen] Mock<HttpContext> httpContext, [Greedy] AccountsController sut)
+    {
+        authService.Setup(x => x.TryLogin(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(false);
+
+        httpContext.Setup(x => x.Request.Headers.Authorization)
+            .Returns(new StringValues(GetAuthorizationHeaderValue()));
+
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var response = await sut.GetAccount(1);
+        response.Should().BeOfType<UnauthorizedResult>();
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task GetAccount_NoAccountWithProvidedId_Returns404([Frozen] Mock<HttpContext> httpContext,
+        [Frozen] Mock<IAccountService> accountService, [Greedy] AccountsController sut)
+    {
+        accountService.Setup(x => x.GetAccountAsync(It.IsAny<int>())).ThrowsAsync(new AccountNotFoundException(1));
+
+        sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var response = await sut.GetAccount(1);
+        response.Should().BeOfType<NotFoundResult>();
+    }
+
+    private string GetAuthorizationHeaderValue(string email = "test@test.com", string password = "password")
+    {
+        var encodedHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{email}:{password}"));
+        var headerValue = $"Basic {encodedHeaderValue}";
+        return headerValue;
     }
 }
