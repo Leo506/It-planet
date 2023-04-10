24c17f5f639acbaaca5fcba8d9eed9f53864532f
Artem Levin <leoartemlev@yandex.ru>
2023-04-10 21:06:53 +0500

    analytic dtos

--- ItPlanet.Domain/Dto/AnalyticDto.cs
+++ ItPlanet.Domain/Dto/AnalyticDto.cs
@@ -2,5 +2,11 @@
 
 public class AnalyticDto
 {
+    public long TotalQuantityAnimals { get; set; }
     
+    public long TotalAnimalsArrived { get; set; }
+    
+    public long TotalAnimalsGone { get; set; }
+
+    public List<AnimalAnalytic> AnimalsAnalytics { get; set; } = new();
 }
---
+++ ItPlanet.Domain/Dto/AnimalAnalytic.cs
@@ -0,0 +1,14 @@
+﻿namespace ItPlanet.Domain.Dto;
+
+public class AnimalAnalytic
+{
+    public string AnimalType { get; set; } = default!;
+
+    public long AnimalTypeId { get; set; } = default!;
+    
+    public long QuantityAnimals { get; set; }
+    
+    public long AnimalsArrived { get; set; }
+    
+    public long AnimalsGone { get; set; }
+}
